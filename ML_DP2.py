bb1fca14ac5a27820789a9c81dd7b816e3cbc99c
nickone96 <109045957+nickone96@users.noreply.github.com>
2023-03-06 20:43:43 -0500

    Add files via upload
    03/06/23- Update
    
    The method used for optimizing the value of C in this code is called Bayesian optimization. More specifically, this code is using Gaussian process-based Bayesian optimization, which is a variant of Bayesian optimization that uses a Gaussian process to model the objective function and generate the next set of points to evaluate. The gp_minimize function from the scikit-optimize library is used to perform the optimization.
    
    In Bayesian optimization, the goal is to find the input (in this case, the value of C) that maximizes the output of an unknown, black-box function (in this case, the test accuracy of the model). The method works by iteratively selecting points in the input space to evaluate, based on a probabilistic model of the objective function that is updated as more points are evaluated. The model is used to balance exploration (trying inputs in new regions of the input space) and exploitation (trying inputs that are likely to be optimal based on the model) in order to find the global optimum of the function with as few evaluations as possible.

--- ML_DP2.py
+++ ML_DP2.py
@@ -1,196 +1,128 @@
 import pandas as pd
-import numpy as np
 from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import OneHotEncoder
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, precision_recall_fscore_support, accuracy_score
 import matplotlib.pyplot as plt
-from sklearn.model_selection import learning_curve
 from sklearn import svm
-from sklearn import preprocessing
 from sklearn.neural_network import MLPClassifier
-from sklearn.neural_network import MLPRegressor
+import numpy as np
+from skopt import gp_minimize
+from skopt.space import Real
+from functools import partial
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
 
 
-
-
-
-
-def train_and_evaluate_svms(C, X_train, y_train, X_test, y_test):
-    score_list_svm = []
-    score_list_svm_rbf = []
-    score_list_svm_poly = []
-
-
-    for i in C:
-        model_svm= svm.SVC(kernel='linear', C=i)
-        model_svm_rbf= svm.SVC(kernel='rbf', C=i)
-        model_svm_poly= svm.SVC(kernel='poly',degree=2, C=i)
-       
-        model_svm.fit(X_train,y_train.ravel())
-        model_svm_rbf.fit(X_train,y_train.ravel())
-        model_svm_poly.fit(X_train,y_train.ravel())
-        
-        score_svm = model_svm.score(X_train, y_train)
-        score_svm_rbf = model_svm_rbf.score(X_train, y_train)
-        score_svm_poly = model_svm_poly.score(X_train, y_train)
-
-        score_list_svm.append(score_svm)
-        score_list_svm_rbf.append(score_svm_rbf)
-        score_list_svm_poly.append(score_svm_poly)
-        
-        
-    print("SVM Training accuracy scores:")
-    for i in range(0, len(C)):
-        print(f'C Value {C[i]:.3g}: {score_list_svm[i]:.3g}')  
-        print(f'C Value {C[i]:.3g}: {score_list_svm_rbf[i]:.3g}')   
-        print(f'C Value {C[i]:.3g}: {score_list_svm_poly[i]:.3g}')
-    print('\n\n')
-        
-        
-
-  
-    #print("(Train) Linear Scores:" , score_list_svm, '\n')
-    #print("(Train) RBF Scores:" , score_list_svm, '\n')
-    #print("(Train) Poly Scores:" , score_list_svm, '\n')
-    
-    
-    score_list_svm_test = []
-    score_list_svm_rbf_test = []
-    score_list_svm_poly_test = []
-    
-    for i in C:
-        model_svm= svm.SVC(kernel='linear', C=i)
-        model_svm_rbf= svm.SVC(kernel='rbf', C=i)
-        model_svm_poly= svm.SVC(kernel='poly',degree=2, C=i)
-       
-        model_svm.fit(X_test,y_test.ravel())
-        model_svm_rbf.fit(X_test,y_test.ravel())
-        model_svm_poly.fit(X_test,y_test.ravel())
-        
-        score_svm = model_svm.score(X_test, y_test)
-        score_svm_rbf = model_svm_rbf.score(X_test, y_test)
-        score_svm_poly = model_svm_poly.score(X_test, y_test)
-
-        score_list_svm_test.append(score_svm)
-        score_list_svm_rbf_test.append(score_svm_rbf)
-        score_list_svm_poly_test.append(score_svm_poly)
-        
-    #print("(Test) Linear Scores:" , score_list_svm_test, '\n')
-    #print("(Test) RBF Scores:" , score_list_svm_test, '\n')
-    #print("(Test) Poly Scores:" , score_list_svm_test, '\n')
-    
-    print("SVM Test accuracy scores:")
-    for i in range(0, len(C)):
-        print(f'C Value {C[i]:.3g}: {score_list_svm_test[i]:.3g}')  
-        print(f'C Value {C[i]:.3g}: {score_list_svm_rbf_test[i]:.3g}')   
-        print(f'C Value {C[i]:.3g}: {score_list_svm_poly_test[i]:.3g}')
-    print('\n\n')
-        
 
 
     
-def logistic_regression(X_train, y_train, X_test, y_test, C):
-    score_list_l2 = []
-    score_list_l1 = []
-    score_list_elasticnet = []
-
-    for i in C:
-        model_l2= LogisticRegression(penalty = 'l2' , max_iter=3000, C=i)
-        model_l1= LogisticRegression(penalty = 'l1' , max_iter=3000, solver='liblinear', C= i )
-        model_elasticnet= LogisticRegression(penalty = 'elasticnet' , max_iter=30000, solver= 'saga', l1_ratio=1, C=i)
-
-        model_l2.fit(X_train,y_train)
-        model_l1.fit(X_train,y_train)
-        model_elasticnet.fit(X_train,y_train)
-
-        score_l2 = model_l2.score(X_train, y_train)
-        score_l1 = model_l1.score(X_train, y_train)
-        score_elasticnet = model_elasticnet.score(X_train, y_train)
-
-        score_list_l2.append(score_l2)
-        score_list_l1.append(score_l1)
-        score_list_elasticnet.append(score_elasticnet)
-        
-    #print("(Train) L2 Scores:" , score_list_l2, '\n')
-    #print("(Train) L1 Scores:" , score_list_l1, '\n')
-    #print("(Train) Elastic Net Scores:" , score_list_elasticnet, '\n')
-    
-    
-    print("Logistic Regression Training accuracy scores:")
-    for i in range(0, len(C)):
-        print(f'C Value {C[i]:.3g}: {score_list_l2[i]:.3g}')  
-        print(f'C Value {C[i]:.3g}: {score_list_l1[i]:.3g}')   
-        print(f'C Value {C[i]:.3g}: {score_list_elasticnet[i]:.3g}')
-    print('\n\n')
-
-    score_list_l2_test = []
-    score_list_l1_test = []
-    score_list_elasticnet_test = []
-
-    for i in C:
-        model_l2= LogisticRegression(penalty = 'l2' , max_iter=3000, C=i)
-        model_l1= LogisticRegression(penalty = 'l1' , max_iter=3000, solver='liblinear', C= i )
-        model_elasticnet= LogisticRegression(penalty = 'elasticnet' , max_iter=30000, solver= 'saga', l1_ratio=1, C=i)
-
-        model_l2.fit(X_test,y_test)
-        model_l1.fit(X_test,y_test)
-        model_elasticnet.fit(X_test,y_test)
-
-        score_l2 = model_l2.score(X_test,y_test)
-        score_l1 = model_l1.score(X_test,y_test)
-        score_elasticnet = model_elasticnet.score(X_test,y_test)
-
-        score_list_l2_test.append(score_l2)
-        score_list_l1_test.append(score_l1)
-        score_list_elasticnet_test.append(score_elasticnet)
-        
-    #print("(Test) L2 Scores:" , score_list_l2_test, '\n')
-    #print("(Test)) L1 Scores:" , score_list_l1_test, '\n')
-    #print("(Test)) Elastic Net Scores:" , score_list_elasticnet_test, '\n')
-    
-    
-    print("Logistic Regression Test accuracy scores:")
-    for i in range(0, len(C)):
-        print(f'C Value {C[i]:.3g}: {score_list_l2_test[i]:.3g}')   
-        print(f'C Value {C[i]:.3g}: {score_list_l1_test[i]:.3g}')       
-        print(f'C Value {C[i]:.3g}: {score_list_elasticnet_test[i]:.3g}')
-    print('\n\n')
-    
-        
-        
-
-
-
-
-
-def run_nn_experiment(C_vals, activation, X_train, y_train, X_test, y_test):
-    acc_train = []
-    acc_test = []
-    for C in C_vals:
-        nn = MLPClassifier(hidden_layer_sizes=(50), activation=activation, solver='adam', max_iter=10000, alpha=C)
+def train_and_evaluate_model(model_type, C_values, X_train, y_train, X_test, y_test):
+    if model_type == 'svm':
+        clf = svm.SVC(C=C_values[0], kernel='linear')
+    elif model_type == 'logistic regression':
+        clf = LogisticRegression(C=C_values[0], solver='lbfgs')
+    elif model_type == 'nn_relu':
+        nn = MLPClassifier(hidden_layer_sizes=(10, 10), activation='relu', alpha=C_values[0], max_iter=1000)
         nn.fit(X_train, y_train.ravel())
-        y_hat_train_nn = nn.predict(X_train)
-        acc_train.append(accuracy_score(y_train, y_hat_train_nn))
-        y_hat_test_nn = nn.predict(X_test)
-        acc_test.append(accuracy_score(y_test, y_hat_test_nn))
-        cm = confusion_matrix(y_test, y_hat_test_nn)
-        disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=nn.classes_)
-        title = "Accuracy Score" 
-        disp.plot()
-        disp.ax_.set_title(title)
-        plt.xlabel(f"Predicted label\nHidden Layer: 50, Activation: {activation}, Iters: 1000, C: {C}\n")
-        plt.show()
-        
-    print(""f"Neural Networks {activation} Training accuracy scores:")
-    for i in range(0, len(C_vals)):
-        print(f'C Value {C_vals[i]:.3g}: {acc_train[i]:.3g}')
-    print('\n\n')
-    print(""f"Neural Networks {activation} Test accuracy scores:")
-    for i in range(0, len(C_vals)):
-        print(f'C Value {C_vals[i]:.3g}: {acc_test[i]:.3g}')
-    print('\n\n')
+        clf = nn
+    elif model_type == 'nn_tanh':
+        nn = MLPClassifier(hidden_layer_sizes=(10, 10), activation='tanh', alpha=C_values[0], max_iter=1000)
+        nn.fit(X_train, y_train.ravel())
+        clf = nn
+    elif model_type == 'nn_logistic':
+        nn = MLPClassifier(hidden_layer_sizes=(10, 10), activation='logistic', alpha=C_values[0], max_iter=1000)
+        nn.fit(X_train, y_train.ravel())
+        clf = nn
+    else:
+        raise ValueError(f'Unknown model type: {model_type}')
+
+    clf.fit(X_train, y_train.ravel())
+    train_acc = clf.score(X_train, y_train)
+    test_acc = clf.score(X_test, y_test)
+    return -test_acc 
+
+    
+    
+
+def run_nn_experiment(C_values, activation, X_train, y_train, X_test, y_test):
+    # convert C_values to a list if it's not already
+    if not isinstance(C_values, list):
+        C_values = [C_values]
+    
+    # perform grid search over the specified values of C
+    best_score = -1
+    for C in C_values:
+        nn = MLPClassifier(hidden_layer_sizes=(100,50), alpha=C, activation=activation, solver='adam', max_iter=1000, random_state=42)
+        nn.fit(X_train, y_train.ravel())
+        score = nn.score(X_test, y_test)
+        if score > best_score:
+            best_score = score
+            best_nn = nn
+    
+    #print(f'Best C for neural network with {activation} activation: {best_nn.alpha:.3g}, best test accuracy: {best_score:.3f}\n')
+    return best_score 
+
+    
+    
+
+def run_nn_experiment(C_values, activation, X_train, y_train, X_test, y_test):
+    # convert C_values to a list if it's not already
+    if not isinstance(C_values, list):
+        C_values = [C_values]
+    
+    # perform grid search over the specified values of C
+    best_score = -1
+    for C in C_values:
+        nn = MLPClassifier(hidden_layer_sizes=(100,50), alpha=C, activation=activation, solver='adam', max_iter=1000, random_state=42)
+        nn.fit(X_train, y_train.ravel())
+        score = nn.score(X_test, y_test)
+        if score > best_score:
+            best_score = score
+            best_nn = nn
+    
+    #print(f'Best C for neural network with {activation} activation: {best_nn.alpha:.3g}, best test accuracy: {best_score:.3f}\n')
+    return best_score
+    
+
+
+
+def optimize_C_for_model(model_type, X_train, y_train, X_test, y_test):
+    C_values = [1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 10, 100]
+    C_values_with_extra = C_values + [1000]
+    best_C = None
+
+    if model_type == 'svm':
+        C_space = Real(1e-7, 100, prior='log-uniform')
+        objective_fn = partial(train_and_evaluate_model, 'svm', X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test)
+    elif model_type == 'logistic regression':
+        C_space = Real(1e-7, 100, prior='log-uniform')
+        objective_fn = partial(train_and_evaluate_model, 'logistic regression', X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test)
+    elif model_type == 'nn_relu':
+        C_space = Real(1e-7, 1000, prior='log-uniform')
+        objective_fn = partial(run_nn_experiment, activation='relu', X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test)
+    elif model_type == 'nn_tanh':
+        C_space = Real(1e-7, 1000, prior='log-uniform')
+        objective_fn = partial(run_nn_experiment, activation='tanh', X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test)
+    elif model_type == 'nn_logistic':
+        C_space = Real(1e-7, 1000, prior='log-uniform')
+        objective_fn = partial(run_nn_experiment, activation='logistic', X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test)
+    else:
+        raise ValueError(f'Unknown model type: {model_type}')
+
+    try:
+        result = gp_minimize(objective_fn, [C_space], n_calls=20, random_state=42)
+        best_C = result.x[0]
+        best_score = -result.fun
+        #print(f'Best C for {model_type}: {best_C:.3g}, best test accuracy: {best_score:.3f}\n')
+    except:
+        print(f'Error occurred during optimization for {model_type}')
+    
+    if best_C is not None:
+        test_score = train_and_evaluate_model(model_type, [best_C], X_train, y_train, X_test, y_test)
+    
+    return best_C
+
 
 
 
@@ -212,27 +144,39 @@ def main():
     
     X_train = preprocessing.scale(X_train)
     X_test = preprocessing.scale(X_test)
-    
 
-    C_NN = [0.0000001, 0.00001, 0.001, 0.01, 0.1, 1, 10, 100, 10000, 1000000]   
-    C = [0.0000001, 0.00001, 0.001, 0.01, 0.1, 1, 10, 100, 10000]   
+ 
+    
+    svm_C = optimize_C_for_model('svm', X_train, y_train, X_test, y_test)
+    print('Best C for SVM:', svm_C)
     
     
-    ##SVM
-    train_and_evaluate_svms(C, X_train, y_train, X_test, y_test)
+    # Support Vector Machines
+    svm_score = train_and_evaluate_model('svm', [svm_C], X_train, y_train, X_test, y_test)
+    print('SVM Score', svm_score * -1)
     
-    ##Logistic Regression   
-    logistic_regression(X_train, y_train, X_test, y_test, C)
-    
+    lr_C = optimize_C_for_model('logistic regression', X_train, y_train, X_test, y_test)
+    print('Best C for Logistic Regression:' , lr_C)
+    # Logistic Regression   
+    lg_score= train_and_evaluate_model('logistic regression', [lr_C], X_train, y_train, X_test, y_test)
+    print('lg_score', lg_score * -1)
     
     ##Neural Networks   
-    run_nn_experiment(C_NN, 'relu', X_train, y_train, X_test, y_test)
-    run_nn_experiment(C_NN, 'tanh', X_train, y_train, X_test, y_test)
-    run_nn_experiment(C_NN, 'logistic', X_train, y_train, X_test, y_test)
+    nn_relu_C = optimize_C_for_model('nn_relu', X_train, y_train, X_test, y_test)
     
+    print('Best C for NN with ReLU:', nn_relu_C)
+    nn_relu = run_nn_experiment(nn_relu_C, 'relu', X_train, y_train, X_test, y_test)
+    print('nn relu score', nn_relu * -1)
     
+    nn_tanh_C = optimize_C_for_model('nn_tanh', X_train, y_train, X_test, y_test)
+    print('Best C for NN with tanh:', nn_tanh_C)
+    nn_tanh = run_nn_experiment(nn_tanh_C, 'tanh', X_train, y_train, X_test, y_test)
+    print('nn tanh score', nn_tanh * -1)
     
-
-
+    nn_logistic_C = optimize_C_for_model('nn_logistic', X_train, y_train, X_test, y_test)
+    print('Best C for NN with logistic:', nn_logistic_C)
+    nn_log= run_nn_experiment(nn_logistic_C, 'logistic', X_train, y_train, X_test, y_test)
+    print('nn log score', nn_log * -1)
+    
 
 main()   
